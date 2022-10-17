690e0f79fa4f1cca752a913493c72510493a6f63
njerris <njerris@users.noreply.github.com>
2022-10-16 22:50:40 -0400

    add api wrapper file + test code

--- bookfinder/src/containers/header/Header.jsx
+++ bookfinder/src/containers/header/Header.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import './header.css'
+import React, { useState } from 'react';
+import { test_endpoint } from '../../api';
+import './header.css';
 
 const Header = () => {
+  // Just for the API test
+  const [testText, setTestText] = useState("");
+  const test_api = async () => {
+    const text = await test_endpoint();
+    setTestText(text);
+  };
+
   return (
     <div className='bookfinder__header section__padding' id="home">
       <div className='bookfinder__header-content'>
@@ -9,10 +17,12 @@ const Header = () => {
 
         <div className='bookfinder__header-content__input'>
           <input type="book" placeholder='Tell us the name of the last book you enjoyed' /> 
-          <button type="button">Get Started </button>
+          <button type="button" onClick={test_api}>Get Started </button>
         </div>
-
       </div>
+      { // Just for the API test
+      testText && <div>API Test Message: {testText}</div>
+      }
     </div>
   )
 };
