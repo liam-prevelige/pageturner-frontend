dd76daa00efbf26f948860b772111c4b947acac3
jpasar <joshua.n.pasaribu.23@dartmouth.edu>
2022-10-23 17:36:32 -0400

    updated search content page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,18 @@
 /*
  * Example major component / page
  */
-import React from 'react';
-import {Header} from './Header';
 
-export const Home = () => {
+import React, { useState } from 'react';
+import { Header } from './Header';
+
+export const Home = () => {    
+  // const [value, setValue] = useState(false);
   return (
     <div className="App">
+      {/* <Header
+        isOn={value}
+        handleToggle={() => setValue(!value)}
+      /> */}
       <div className='gradient_bg'>
         <Header />
       </div>
