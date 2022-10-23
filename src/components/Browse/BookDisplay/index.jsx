3a50d439ba5c1a30753a43932649eeb71c96acec
Cameron Raker <cameronraker@Cams-MacBook-Pro.local>
2022-10-23 17:58:06 -0400

    Got beginnings of grid system working. Added progress bar. Resized image and text.

--- src/components/Browse/BookDisplay/index.jsx
+++ src/components/Browse/BookDisplay/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 export const BookDisplay = () => {
   return (
-    <div>
-      <img src="goblet.jpg" alt="Harry Potter"/>
-      <h3>
-        Harry Potter and the Goblet of Fire
-      </h3>
+    <div className="mb-3">
+      <img src="goblet.jpg" alt="Harry Potter" width="150" height="210"/>
       <h5>
-        JK Rowling. Match percentage: 100%
+        Harry Potter and the Goblet of Fire
       </h5>
+      <h6>
+        JK Rowling
+      </h6>
+      <ProgressBar animated variant="success" now="90" label="90%" />
     </div>
   );
 };
--- src/components/Browse/index.jsx
+++ src/components/Browse/index.jsx
@@ -5,7 +5,24 @@ import {BookDisplay} from './BookDisplay';
 export const Browse = () => {
   return (
     <div>
-      <BookDisplay />
+      <div className="container-sm text-center">
+        <div className="row align-items-start">
+          <div className="col-2">
+            <BookDisplay/>
+          </div>
+          <div className="col-2">
+            <BookDisplay/>
+          </div>
+        </div>
+        <div className="row align-items-start">
+          <div className="col-2">
+            <BookDisplay/>
+          </div>
+          <div className="col-2">
+            <BookDisplay/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
