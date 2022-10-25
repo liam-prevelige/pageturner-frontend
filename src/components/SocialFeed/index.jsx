18e5ac50395cadb9e10e397c841546a59c23d5ed
ajk7789 <alexander.j.kruger.23@dartmouth.edu>
2022-10-25 18:29:27 -0400

    set up page so feed scrolls but exterior columns do not

--- src/components/SocialFeed/index.jsx
+++ src/components/SocialFeed/index.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import './socialFeed.css';
+
+import {SocialPost} from './SocialPost';
+
 export const SocialFeed = () => {
   return (
-    <div>
-      Social Feed
-    </div>
+    <Container fluid>
+      <Row>
+        <Col> {/** fixed */}
+          <div className='disable-scroll'>
+            <h1>Fixed</h1>
+          </div>
+        </Col>
+        <Col> {/** scrollable - Alex's work goes in this column */}
+          <SocialPost/>
+          <SocialPost/>
+          <SocialPost/>
+          <SocialPost/>
+          <SocialPost/>
+          <SocialPost/>
+        </Col>
+        <Col> {/** fixed */}
+          <div className='disable-scroll'>
+            <h1>Fixed</h1>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
