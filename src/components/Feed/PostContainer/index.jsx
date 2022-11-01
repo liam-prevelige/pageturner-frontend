a7fd384af37c8772f847436dde35112548b653c4
ajk7789 <alexander.j.kruger.23@dartmouth.edu>
2022-11-01 14:08:36 -0400

    recognizes when the user scrolls down near the bottom

--- src/components/Feed/PostContainer/index.jsx
+++ src/components/Feed/PostContainer/index.jsx
@@ -7,37 +7,70 @@ import {Post} from './Post/post';
 import {getFeed} from '../../../api';
 import {useTrackVisibility} from 'react-intersection-observer-hook';
 
-
 export const PostContainer = () => {
   const [posts, setPosts] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [triggerNumber, setTriggerNumber] = useState(7);
+  const [page, setPage] = useState(0);
 
-  const [
-    ref,
-    {isVisible},
-  ] = useTrackVisibility();
+  const [ref, {isVisible}] = useTrackVisibility();
 
   const load = async () => {
-    if (!loaded) {
-      const results = await getFeed();
-      setPosts(results);
-      setLoaded(true);
-    }
+    const results = await getFeed();
+    setPosts(results);
+    setLoaded(true);
+  };
+
+  const incrementPage = () => {
+    const pageTemp = page;
+    setPage(pageTemp + 1);
+  };
+
+  const addPosts = async () => {
+    // try {
+    //   incrementPage();
+    //   const tempTriggerNumber = triggerNumber;
+    //   setTriggerNumber(tempTriggerNumber + 10);
+    //   const results = await getFeed();
+    //   const tempPosts = posts;
+    //   tempPosts.concat(results);
+    //   setPosts(tempPosts);
+    // } catch (err) {
+    //   console.log(err);
+    // };
+    incrementPage();
+    const tempTriggerNumber = triggerNumber;
+    setTriggerNumber(tempTriggerNumber + 10);
+    const results = await getFeed();
+    console.log(posts);
+    console.log(results);
+    const tempPosts = posts;
+    tempPosts.concat(results);
+    setPosts(tempPosts);
   };
 
   useEffect(() => {
-    load();
-    console.log(`The component is ${isVisible ? 'visible' : 'not visible'}.`);
+    if (!loaded) {
+      load();
+    } else {
+      addPosts();
+      console.log(posts.length);
+      const tempTriggerNumber = triggerNumber;
+      setTriggerNumber(tempTriggerNumber + 10);
+    }
   }, [isVisible]);
 
   return (
     <div className='feed'>
-
       {
         // eslint-disable-next-line arrow-parens
-        posts.map(post => (
+        posts.map((post, index) => (
           // eslint-disable-next-line react/jsx-key
-          <Post ref={ref} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+          <div>
+            {/* <PostList isVisible={isVisible}/> */}
+            {index == triggerNumber ? <div ref={ref}></div> : null}
+            <Post key={index} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+          </div>
         ))
       }
     </div>
