0286ab5c546021082907d409a62c1ac27b9468dd
njerris <njerris@users.noreply.github.com>
2022-11-14 15:28:29 -0500

    add people search page, friends

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import {LandingPage} from './components/LandingPage';
+import {People} from './components/People';
 import {Feed} from './components/Feed';
 import {Profile} from './components/Profile';
 import Auth from './components/Auth/Auth';
@@ -41,6 +42,7 @@ const App = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/feed">Feed</Nav.Link>
+            <Nav.Link href="/people">People</Nav.Link>
             {loggedIn && <Nav.Link href="/profile">My Profile</Nav.Link>}
           </Nav>
           <Nav className="navbar-right navbar-custom1">
@@ -53,6 +55,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/feed" element={<Feed />} />
+        <Route path="/people" element={<People />} />
         {loggedIn && <Route path="/profile" element={<Profile />}/>}
       </Routes>
     </Router >
--- src/api.js
+++ src/api.js
@@ -18,6 +18,81 @@ export async function onLogin() {
   // We don't care about the result here
 }
 
+// Search for a user
+export const searchUsers = async (query) => {
+  const response = await fetch(`${API_URL}/user/search/${query}`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application.json',
+      'Content-Type': 'application/json',
+    },
+    cache: 'default',
+  });
+  const body = await response.json();
+  if (!response.ok) {
+    throw new Error('Call to /search failed');
+  }
+
+  return body.result;
+};
+
+// Get a user's books
+export const getUserBooks = async (email) => {
+  const response = await fetch(`${API_URL}/user/books/${email}`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application.json',
+      'Content-Type': 'application/json',
+    },
+    cache: 'default',
+  });
+  const body = await response.json();
+
+  if (!response.ok) {
+    throw new Error('Call to /feed/get failed');
+  }
+
+  return body;
+};
+
+/**
+ * Add a friend
+ * @param {string} email - email of user to add as a friend
+ * @return {boolean} success
+ */
+export async function addFriend(email) {
+  const response = await fetch(`${API_URL}/user/friend/add`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': sessionStorage.getItem('auth_token'),
+    },
+    body: JSON.stringify({friend_email: email}),
+  });
+  const body = await response.json();
+  return body.success;
+}
+
+/**
+ * Remove a friend
+ * @param {string} email - email of user to add as a friend
+ * @return {boolean} success
+ */
+export async function removeFriend(email) {
+  const response = await fetch(`${API_URL}/user/friend/add`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': sessionStorage.getItem('auth_token'),
+    },
+    body: JSON.stringify({friend_email: email}),
+  });
+  const body = await response.json();
+  return body.success;
+}
+
 export const getFeed = async () => {
   const response = await fetch(`${API_URL}/feed/get`, {
     method: 'GET',
