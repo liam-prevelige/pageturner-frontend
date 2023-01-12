016a984e14e9c008b3134ec7bef60d4991432896
liam-prevelige <liam.r.prevelige.23@dartmouth.edu>
2023-01-12 14:05:24 -0500

    Hotfix: routing change for UI bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/assets/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
