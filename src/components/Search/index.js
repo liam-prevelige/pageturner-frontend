9f3b16be701bca13c40889052f6951bfaa0a6e2c
njerris <njerris@users.noreply.github.com>
2023-01-28 15:56:04 -0500

    fix structure of search component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import SearchBox from '../../assets/Icons';
+import SearchIcon from '../../assets/Icons';
 
 export const Search = () => {
-  return <SearchBox />;
+  return (<div>
+    <div className="flex items-center space-x-5 p-3 m-3 rounded-full bg-slate-200 text-black focus-within:ring-2 focus-within:ring-primary-button focus:ring-1">
+      <SearchIcon />
+      <div>
+        <input className="focus:outline-none bg-transparent w-full" type="text" placeholder="Search PageTurner"/>
+      </div>
+    </div>
+  </div>);
 };
