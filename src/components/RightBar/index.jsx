fa5257559a2f844a8f2f809e62f0beacc32d1de4
camraker6 <cameron.s.raker.23@dartmouth.edu>
2023-01-31 16:37:57 -0500

    Quick change - got rid of WhoFollow right box

--- src/components/RightBar/index.jsx
+++ src/components/RightBar/index.jsx
@@ -1,13 +1,11 @@
 import {React} from 'react';
 
 import {Trending} from './Trending';
-import {WhoFollow} from './WhoFollow';
 
 export const RightBar = () => {
   return (
     <>
       <Trending />
-      <WhoFollow />
     </>
   );
 };
