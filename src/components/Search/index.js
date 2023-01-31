5386a71150053645f9bb0a6fa940d037141133b8
camraker6 <cameron.s.raker.23@dartmouth.edu>
2023-01-31 17:26:21 -0500

    Moved tabs outside of the search box

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -26,17 +26,19 @@ export const Search = () => {
     }
   };
 
-  return (<div className="flex items-center space-x-5 p-3 m-3 rounded-full bg-slate-200 text-black focus-within:ring-2 focus-within:ring-primary-button focus:ring-1">
-    <SearchIcon />
-    <div>
-      <input
-        className="focus:outline-none bg-transparent w-full"
-        type="text"
-        placeholder="Search PageTurner"
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
+  return (<div>
+    <div className="flex items-center space-x-5 p-3 m-3 rounded-full bg-slate-200 text-black focus-within:ring-2 focus-within:ring-primary-button focus:ring-1">
+      <SearchIcon />
+      <div>
+        <input
+          className="focus:outline-none bg-transparent w-full"
+          type="text"
+          placeholder="Search PageTurner"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+      </div>
     </div>
     {show && <div>
       <ChakraProvider resetCSS={false}>
