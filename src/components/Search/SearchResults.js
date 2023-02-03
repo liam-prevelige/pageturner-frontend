c0de5b3395b564e8d61c28db32133c9522801e05
camraker6 <cameron.s.raker.23@dartmouth.edu>
2023-02-02 21:54:32 -0500

    Books now displayed

--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -89,40 +89,21 @@ export const GroupSearchResult = (groupInfo) => {
 export const BookSearchResult = ({bookInfo}) => {
   const navigate = useNavigate();
 
-  const [userInfo, setUser] = useState(null);
 
-  const loadUserProfile = (uid) => {
-    const path = `/profile?uid=${uid}`;
+  const loadBook = (uid) => {
+    const path = `/profile?id=${uid}`;
     navigate(path);
   };
-
-  const loadBookshelf = (id) => {
-    const path = `/profile?id=${id}`;
-    navigate(path);
-  };
-
-  const getUser = async (uid) => {
-    if (!uid) return;
-    // Turn on loading state while waiting for API
-    setUser(null);
-    // TODO: not returning right profile
-    const profile = await getProfile(uid);
-    setUser(profile);
-  };
-
-  useEffect(() => {
-    getUser(bookshelfInfo.ownerId);
-  }, [bookshelfInfo]);
 
   return (
-    (userInfo != null) && (<div className='flex space-x-3 px-4 py-3 border-primary-container_border_color'>
-      <img src={userInfo.profilePicture} className="cursor-pointer w-11 h-11 rounded-full" onClick={() => loadUserProfile(userInfo._id)} />
+    <div className='flex space-x-3 px-4 py-3 border-primary-container_border_color'>
+      <img src={bookInfo.volumeInfo.imageLinks.smallThumbnail} className="cursor-pointer w-11 h-11" onClick={() => loadBook(bookInfo.id)} />
       <div className="flex-1">
-        <div className="flex items-center text-sm space-x-2 cursor-pointer" onClick={() => loadBookshelf(bookshelfInfo._id)}>
-          <span className="ml-1 font-bold text-black">{bookshelfInfo.name}</span>
-          <span className="ml-2 text-primary-gray_colors">@{userInfo.tag}</span>
+        <div className="flex items-center text-sm space-x-2 cursor-pointer" onClick={() => loadBook(bookInfo.id)}>
+          <span className="ml-1 font-bold text-black">{bookInfo.volumeInfo.title}</span>
+          <span className="ml-2 font-bold text-black">by {bookInfo.volumeInfo.authors[0]}</span>
         </div>
       </div>
-    </div>)
+    </div>
   );
 };
