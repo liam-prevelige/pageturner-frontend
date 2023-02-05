d21b73f49704b8ebbcb85ff4fc057040d1c7ed8a
njerris <njerris@users.noreply.github.com>
2023-02-05 17:44:59 -0500

    auto change tab to one with most matching results

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -11,17 +11,45 @@ export const Search = () => {
   const [searchInput, setSearchInput] = useState('');
   const [results, setResults] = useState(null);
   const [show, setShow] = useState(false);
+  const [tabIndex, setTabIndex] = useState(0);
 
+  // Handler for text change in search bar
   const handleInputChange = async (newInput) => {
+    // Update search bar text
     setSearchInput(newInput);
+
+    // Hide results until the user hits enter
     setShow(false);
+
+    // Clear results to null
     setResults(null);
   };
 
+  // Hit search endpoint and manage state until results are available
   const performSearch = async () => {
+    // Clear results to null
     setResults(null);
+
+    // Show results (but will actually just show the loading icon for now)
     setShow(true);
+
+    // Get search results for all content
     const res = await searchContent(searchInput);
+
+    // Determine which tab to show by default
+    let highestResCount = 0;
+    let initIndex = 0;
+    // IMPORTANT: the order here must match the order of tabs defined in JSX
+    const types = ['users', 'groups', 'bookshelves', 'comments', 'books'];
+    types.forEach((type, index) => {
+      if (res[type].length > highestResCount) {
+        highestResCount = res[type].length;
+        initIndex = index;
+      }
+    });
+    setTabIndex(initIndex);
+
+    // Set results (will change from loading icon to results)
     setResults(res);
   };
 
@@ -50,7 +78,14 @@ export const Search = () => {
       <ReactLoading type="spin" color="black" />
     </div> : <div>
       <ChakraProvider resetCSS={false}>
-        <Tabs isFitted className="m-3" variant='line' colorScheme='cyan'>
+        <Tabs
+          isFitted
+          className="m-3"
+          variant='line'
+          colorScheme='cyan'
+          index={tabIndex}
+          onChange={(index) => setTabIndex(index)}
+        >
           <TabList>
             <Tab>People</Tab>
             <Tab>Groups</Tab>
@@ -60,29 +95,29 @@ export const Search = () => {
           </TabList>
           <TabPanels>
             <TabPanel>
-              {results.users.map((user, index) => (<Row key={index}>
+              {results.users.length ? results.users.map((user, index) => (<Row key={index}>
                 <UserSearchResult userInfo={user}/>
-              </Row>))}
+              </Row>)) : <Row>No Results</Row>}
             </TabPanel>
             <TabPanel>
-              {results.groups.map((group, index) => (<Row key={index}>
+              {results.groups.length ? results.groups.map((group, index) => (<Row key={index}>
                 <GroupSearchResult groupInfo={group}/>
-              </Row>))}
+              </Row>)): <Row>No Results</Row>}
             </TabPanel>
             <TabPanel>
-              {results.bookshelves.map((bookshelf, index) => (<Row key={index}>
+              {results.bookshelves.length ? results.bookshelves.map((bookshelf, index) => (<Row key={index}>
                 <BookshelfSearchResult bookshelfInfo={bookshelf}/>
-              </Row>))}
+              </Row>)): <Row>No Results</Row>}
             </TabPanel>
             <TabPanel>
-              {results.comments.map((comment, index) => (<Row key={index}>
+              {results.comments.length ? results.comments.map((comment, index) => (<Row key={index}>
                 <Comment commentId={comment._id} noParent={true}/>
-              </Row>))}
+              </Row>)): <Row>No Results</Row>}
             </TabPanel>
             <TabPanel>
-              {results.books.map((book, index) => (<Row key={index}>
+              {results.books.length ? results.books.map((book, index) => (<Row key={index}>
                 <BookSearchResult bookInfo={book}/>
-              </Row>))}
+              </Row>)) : <Row>No Results</Row>}
             </TabPanel>
           </TabPanels>
         </Tabs>
