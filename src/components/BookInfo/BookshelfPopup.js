5a1b2806190c21fef81d2d226bca4024aed75550
liam-prevelige <liam.r.prevelige.23@dartmouth.edu>
2023-02-08 18:55:45 -0500

    improved add to bookshelf popup UX

--- src/components/BookInfo/BookshelfPopup.js
+++ src/components/BookInfo/BookshelfPopup.js
@@ -1,20 +1,69 @@
-import React, {useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import {
   FormControl,
-  FormLabel,
   Input,
 } from '@chakra-ui/react';
+import {getBookshelves} from '../../api';
 
 export const BookshelfPopup = () => {
   const [show, setShow] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [bookshelves, setBookshelves] = useState([]);
+  const profile = useState(JSON.parse(sessionStorage.getItem('profile')))[0];
+  const [selectedBookshelf, setSelectedBookshelf] = useState('Choose from existing');
+  const [newBookshelfName, setNewBookshelfName] = useState('');
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const dropdownClass = `dropdown-menu${showDropdown ? ' show' : ''}`;
+
+  const toggleShowDropdown = (e) => {
+    e.stopPropagation();
+    setShowDropdown(!showDropdown);
+  };
+
+  const updateSelectedBookshelf = (name) => {
+    setSelectedBookshelf(name);
+    setShowDropdown(false);
+  };
+
+  const updateNewBookshelfName = (e) => {
+    console.log(e.target);
+    setNewBookshelfName(e.target.value);
+    if (e.target.value.length > 0) {
+      setSelectedBookshelf('Choose from existing'); // Reset selected bookshelf
+    }
+  };
+
+  const fetchBookshelves = async () => {
+    if (!profile) return;
+    // TODO: stop sequential fetching
+    // TODO: add ability for book club
+    const loadedBookshelves = await getBookshelves(profile._id, 'user');
+    // console.log(bookshelfIds);
+    // const loadedBookshelves = [];
+    // for (const bookshelfId of bookshelfIds) {
+    //   const bookshelfData = await getBookshelf(bookshelfId);
+    //   loadedBookshelves = [...loadedBookshelves, bookshelfData];
+    //   console.log(bookshelfData);
+    // }
+    // console.log('loadedBookshelves', loadedBookshelves);
+    setBookshelves(loadedBookshelves);
+  };
+
+  const collapseDropdown = () => {
+    // If you click anywhere else on modal, collapse dropdown
+    setShowDropdown(false);
+  };
+
+  useEffect(() => {
+    fetchBookshelves();
+  }, []);
 
   return (
-    <>
+    <div onClick={collapseDropdown}>
       <button className="font-bold w-36 mt-3 mr-3 text-primary-button wrap-text text-sm rounded-full shadow-sm py-2 border-2 border-primary-button transform transition-colors duration-200 hover:bg-primary-button hover:border-primary-button hover:text-white" onClick={handleShow}>
           Add to Bookshelf
       </button>
@@ -27,13 +76,16 @@ export const BookshelfPopup = () => {
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <div className="dropdown">
-                <button className="font-bold p-3 text-slate-500 wrap-text text-sm rounded-full shadow-sm py-2 border-2 border-slate-500 transform transition-colors duration-200 hover:bg-slate-500 hover:text-white dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Choose from existing
+                <button className="font-bold p-3 text-slate-500 wrap-text text-sm rounded-full shadow-sm py-2 border-2 border-slate-500 transform transition-colors duration-200 dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onClick={(e) => toggleShowDropdown(e)}>
+                  {selectedBookshelf}
                 </button>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a className="dropdown-item" href="#">Action</a>
-                  <a className="dropdown-item" href="#">Another action</a>
-                  <a className="dropdown-item" href="#">Something else here</a>
+                <div className={dropdownClass} aria-labelledby="dropdownMenuButton">
+                  {bookshelves && bookshelves.map((bookshelfData, index) =>
+                    (<div className="cursor-pointer ml-2 mb-2" key={index} onClick={() => updateSelectedBookshelf(bookshelfData.name)}>
+                      {bookshelfData.name}
+                      <div className="border-b mr-3 mt-2 border-slate-300"/>
+                    </div>
+                    ))}
                 </div>
               </div>
             </Form.Group>
@@ -43,21 +95,20 @@ export const BookshelfPopup = () => {
             >
               <Form.Label>Or create new:</Form.Label>
               <FormControl>
-                <FormLabel htmlFor='name'>New Bookshelf</FormLabel>
-                <Input placeholder='Title' id='name' onChange={(e) => setName(e.target.value)} />
+                <Input className="border border-slate-300 rounded-full pt-2 pb-2 pl-4" placeholder='Bookshelf Title' id='name' value={newBookshelfName} onChange={(e) => updateNewBookshelfName(e)} />
               </FormControl>
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <button className="font-bold w-16 mt-3 mr-3 text-red-400 wrap-text text-sm rounded-full shadow-sm py-2 hover:bg-red-600 hover:text-white border-2 border-red-600 transform transition-colors duration-200" onClick={handleClose}>
+          <button className="font-bold w-16 mr-3 text-red-400 wrap-text text-sm rounded-full shadow-sm py-2 hover:bg-red-600 hover:text-white border-2 border-red-600 transform transition-colors duration-200" onClick={handleClose}>
             Cancel
           </button>
-          <button className="font-bold w-36 mt-3 mr-3 text-primary-button wrap-text text-sm rounded-full shadow-sm py-2 border-2 border-primary-button transform transition-colors duration-200 hover:bg-primary-button hover:border-primary-button hover:text-white" onClick={handleClose}>
+          <button className="font-bold w-36 mr-3 text-primary-button wrap-text text-sm rounded-full shadow-sm py-2 border-2 border-primary-button transform transition-colors duration-200 hover:bg-primary-button hover:border-primary-button hover:text-white" onClick={handleClose}>
             Add to Bookshelf
           </button>
         </Modal.Footer>
       </Modal>
-    </>
+    </div>
   );
 };
