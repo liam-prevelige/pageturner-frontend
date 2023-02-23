15bb07879b238972c3c6e4c59dccbc420941198f
jpasar <joshua.n.pasaribu.23@dartmouth.edu>
2023-02-22 21:54:59 -0500

    add onboarding skeleton

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,26 +1,55 @@
 import {React, useState, useEffect} from 'react';
 import {Search} from '../Search';
 import {ShareBox} from '../ShareBox/ShareBox';
-// import {ThreadView} from '../ThreadView/ThreadView';
 import {Timeline} from '../Comment/Timeline';
 import {Switch, ChakraProvider} from '@chakra-ui/react';
 import {FaGlobeAmericas, FaUser} from 'react-icons/fa';
 import ReactGA from 'react-ga';
+import introJs from 'intro.js';
+import 'intro.js/introjs.css';
 
 export const HomePage = () => {
   const profile = useState(JSON.parse(sessionStorage.getItem('profile')))[0];
+
   useEffect(() => {
     ReactGA.pageview(window.location.pathname);
+
+    // Initialize Intro.js and define the steps for the pop-ups
+    introJs()
+        .setOptions({
+          steps: [
+            {
+              title: 'Welcome to Pageturner!',
+              intro: 'Thank you for visiting our site. Our goal is to promote reading by fostering meaningful interactions centered around books.',
+              position: 'bottom',
+            },
+            {
+              element: document.querySelector('.profilepage'),
+              title: 'Your profile',
+              intro: 'Your profile is a space to highlight your personal information, posts, reviews, bookshelves, and likes. When you\'re ready to edit your profile, simply click on this tab.',
+              position: 'right',
+            },
+            {
+              element: document.querySelector('.searchbar'),
+              title: 'Search for content',
+              intro: 'Discover fascinating content with our search bar! Search results are divided into People, Groups, Bookshelves, Comments, and Books. Give it a try and find your first book!',
+              position: 'bottom',
+            },
+          ],
+        })
+        .start();
   }, []);
+
   const changeTimeline = (e) => {
     window.dispatchEvent(new CustomEvent('timelineChange', {detail: e.target.checked}));
   };
+
   return (
     <>
-      <div className="min-h-screen max-w-7xl flex">
+      <div className="mainpage min-h-screen max-w-7xl flex">
         <main className="flex-1 flex flex-col">
           <>
-            <Search />
+            <Search className='searchbar'/>
             <div className="h-full border-r w-full border-l">
               <div className="flex flex-row w-full justify-between border-slate-300 border-b border-slate-300 p-4">
                 <div className="bg-white font-bold text-3xl text-black">
@@ -35,7 +64,7 @@ export const HomePage = () => {
                 </div>
               </div>
               <div className="relative z-20 flex space-x-4 px-4 py-2 border-b border-slate-300">
-                <ShareBox />
+                <ShareBox className='profilepage'/>
               </div>
               <div className='relative z-10'>
                 <Timeline />
