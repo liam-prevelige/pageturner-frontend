1a577a566a8d51a5c64dd2e1cda1da77ce47f285
liam-prevelige <liam.r.prevelige.23@dartmouth.edu>
2023-02-25 19:28:06 -0500

    added https redirect

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import {createRoot} from 'react-dom/client';
 import App from './App';
 import './index.css';
 import {GoogleOAuthProvider} from '@react-oauth/google';
+import HttpsRedirect from 'react-https-redirect';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-    <GoogleOAuthProvider clientId="556168228068-60hp84a7hnkqoh1i8vs2m2vakff2a7ae.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>);
+    <HttpsRedirect>
+      <GoogleOAuthProvider clientId="556168228068-60hp84a7hnkqoh1i8vs2m2vakff2a7ae.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
+    </HttpsRedirect>);
